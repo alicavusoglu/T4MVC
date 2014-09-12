c7c7b1491f23f6915c743e8ee6c0a388cda333f4
alicavusoglu <alicavusoglu07@hotmail.com>
2014-09-12 10:06:59 +0300

    T4MVC
    T4MVC

---
+++ T4MVC/T4MVCSample/T4MVCSample/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace T4MVCSample.Controllers
+{
+    public partial class HomeController : Controller
+    {
+        public virtual ActionResult Index()
+        {
+            return View();
+        }
+
+        public virtual ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return RedirectToAction(MVC.Home.ActionNames.Contact, MVC.Home.Name);
+        }
+
+        public virtual ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
